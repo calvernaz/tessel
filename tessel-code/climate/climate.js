194bcfc89b363554d119ff14c62b53e2eccc1d82
Cesar Alvernaz <cesar.alvernaz@gmail.com>
2016-11-17 22:35:48 +0000

    Using mosquitto to publish sensor values

--- tessel-code/climate/climate.js
+++ tessel-code/climate/climate.js
@@ -1,22 +1,24 @@
 var tessel = require('tessel');
+var mqtt = require('mqtt')
 var climatelib = require('climate-si7020');
+var climate = climatelib.use(tessel.port['B']);
+var client  = mqtt.connect('tcp://192.168.1.10')
 
-var climate = climatelib.use(tessel.port['A']);
+var conf = require('./env.json')
 
 climate.on('ready', function () {
-		console.log('Connected to climate module');
+    setImmediate(function loop () {
+	climate.readTemperature(function (err, temp) {
+	    climate.readHumidity(function (err, humid) {
+		client.publish(conf.broker.topic, 
+			       JSON.stringify({ "cid": conf.sensor.cid, "value": temp.toFixed(2) }));
+		setTimeout(loop, 60000);
 
-		// Loop forever
-		setImmediate(function loop () {
-				climate.readTemperature(function (err, temp) {
-						climate.readHumidity(function (err, humid) {
-								console.log('Degrees:', temp.toFixed(4) + 'C', 'Humidity:', humid.toFixed(4) + '%RH');
-								setTimeout(loop, 300);
-						});
-				});
-		});
+	    });
+	});
+    });
 });
 
 climate.on('error', function(err) {
-		console.log('error connecting module', err);
+    console.log('error connecting module', err);
 });
