b269d43411b45e31c2ab9e59954ba13c2616f052
Cesar Alvernaz <cesar.alvernaz@gmail.com>
2016-11-23 21:39:13 +0000

    Remove old file. Use only temperature

--- tessel-code/climate/climate.js
+++ tessel-code/climate/climate.js
@@ -1,6 +1,8 @@
 var tessel = require('tessel');
 var mqtt = require('mqtt')
 var climatelib = require('climate-si7020');
+
+
 var climate = climatelib.use(tessel.port['B']);
 var client  = mqtt.connect('tcp://192.168.1.10')
 
@@ -9,12 +11,15 @@ var conf = require('./env.json')
 climate.on('ready', function () {
     setImmediate(function loop () {
 	climate.readTemperature(function (err, temp) {
-	    climate.readHumidity(function (err, humid) {
-		client.publish(conf.broker.topic, 
-			       JSON.stringify({ "cid": conf.sensor.cid, "value": temp.toFixed(2) }));
-		setTimeout(loop, 60000);
-
-	    });
+	    client.publish(conf.broker.topic, 
+			   JSON.stringify({ "cid": conf.sensor.cid, 
+					    "sensors": [{
+						"name": "temperature",
+						"idx": 1,
+						"value": temp.toFixed(2)
+					    }]
+					  }));
+	    setTimeout(loop, 60000);
 	});
     });
 });
